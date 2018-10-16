d302ffdc2a765a799faf9557c00038c0e04f0a9d
scanner-jiangbiao <903887001@qq.com>
2018-10-16 20:37:02 +0800

    修改病员诊断管理的添加功能中诊断和术式以element的Cascader组件获取数据的方式

--- src/views/jdyy/patients/jdyyvis/jdyyVisAdd.vue
+++ src/views/jdyy/patients/jdyyvis/jdyyVisAdd.vue
@@ -1,63 +1,38 @@
 <template lang="pug">
   kalix-dialog.user-add(title='添加' bizKey="jdyyVis" ref="kalixBizDialog" v-bind:formModel.sync="formModel" v-bind:targetURL="targetURL")
     div.el-form(slot="dialogFormSlot")
-      <!--div.container_box-->
-      <!--el-form(:model="formModel" :rules="rules" ref="formModel")-->
-        <!--div.start-->
-          <!--div.start1-->
       el-form-item(label="患者" prop="pid" v-bind:rules="rules.pid" v-bind:label-width="labelWidth")
         kalix-select(v-model="formModel.pid" v-bind:requestUrl="JdyypatientsURL" appName="pid" id="name" placeholder="请选择患者")
       el-form-item(label="诊断" prop="diagnosis" v-bind:label-width="labelWidth" v-bind:rules="rules.diagnosis")
-        kalix-font-diacascader.Border(v-model="formModel.diagnosis" v-on:change="getModelDia")
+        <!--kalix-font-diacascader.Border(v-model="formModel.diagnosis" v-on:change="getModelDia")-->
+        el-cascader(v-model="formModel.diagnosis" placeholder="请选择诊断信息" :options="options" filterable change-on-select)
       el-form-item(label="术式" prop="surgical" v-bind:label-width="labelWidth" v-bind:rules="rules.surgical")
-        kalix-font-surcascader.Border(v-model="formModel.surgical" v-on:change="getModelSur")
+        <!--kalix-font-surcascader.Border(v-model="formModel.surgical" v-on:change="getModelSur")-->
+        el-cascader(v-model="formModel.surgical" placeholder="请选择术式信息" :options="items" filterable change-on-select)
       el-form-item.short(label="手术日期" prop="dateOperation" v-bind:label-width="labelWidth" v-bind:rules="rules.dateOperation")
         kalix-datepicker-simple(v-model="formModel.dateOperation" type="datetime" placeholder="选择日期" format="yyyy-MM-dd" style="width: 100%;")
-      <!--el-form-item(label="分期" prop="periodization" v-bind:label-width="labelWidth" v-bind:rules="rules.periodization")-->
-        <!--el-select(v-model="formModel.periodization")-->
       el-form-item.short.toleft(label="分期" prop="periodization" v-bind:label-width="labelWidth" v-bind:rules="rules.periodization")
         el-select(v-model="formModel.periodization" placeholder="请选择")
           el-option(label="内科" value="外科")
           el-option(label="外科" value="外科")
-
-      <!--div.box-->
-        <!--div.contions-->
-          <!--kalix-clansman-upload(:action="action" v-on:filePath="getFilePath"-->
-            <!--v-on:selectChange="setGroup" :fileList="fileList" fileType="img" tipText="只能上传jpg/png文件，且不超过2MB"-->
-          <!--)-->
-      <!--div.bottom-->
-        <!--div.bottom-box-->
-          <!--ul.right_ul-->
-            <!--li.right_li-->
-              <!--el-button.button-save( size="large" v-on:click="submitForm") 保存-->
-            <!--li.right_li-->
-              <!--el-button.button-save( size="large" v-on:click="resetForm") 重置-->
 </template>
 
 <script type="text/ecmascript-6">
-  import {JdyyvisitURL, JdyysurURL, JdyypatientsURL} from '../../config.toml'
+  import {JdyyvisitURL, JdyysurURL, JdyypatientsURL, JdyydiaURL} from '../../config.toml'
   import FormModel from './model'
-  import {baseURL} from '../../../../config/global.toml'
-  import KalixClansmanUpload from '../../../../components/fileUpload/upload'
-  import KalixFontCascader from '../../../../components/cascader/ThreeCascader'
-  import KalixDatepickerSimple from '../../../../components/corelib/components/common/baseDatepicker'
-  import KalixFontSurcascader from '../../../../components/cascader/SurThreeCascader'
-  import KalixFontDiacascader from '../../../../components/cascader/DiaThreeCascader'
-  import KalixSelect from '../../../../components/corelib/components/common/baseSelect'
   export default {
     name: 'JdyyVisAdd',
-    components: {
-      KalixSelect, KalixFontSurcascader, KalixFontDiacascader, KalixDatepickerSimple, KalixFontCascader, KalixClansmanUpload },
+    components: {},
     data() {
       return {
         downloadURL: JdyyvisitURL,
         formModel: Object.assign({}, FormModel),
         labelWidth: '120px',
         fileList: [],
-        action: baseURL + '/camel/rest/upload',
         columnParam: undefined,
         albumname: '',
         options: [],
+        items: [],
         filePathArr: [],
         fileNameArr: [],
         rules: {
@@ -70,10 +45,13 @@
         JdyypatientsURL: JdyypatientsURL
       }
     },
+    mounted() {
+      this.getDiaCascader()
+      this.getSurCascader()
+    },
     methods: {
       init(dialogOption) {
         console.log('---------dialogOption------------', dialogOption)
-        console.log('qqqqqqq-===ddddddddddddddddd======')
       },
       open() { // 添加术式成功提示
         this.$alert(JdyysurURL, '术式', {
@@ -86,45 +64,11 @@
           }
         })
       },
-      // getFilePath(filePath, fileName) { // 图片上传路径
-      //   console.log('--getFilePath---', filePath)
-      //   console.log('--fileName---', fileName)
-      //   this.filePathArr.push(filePath)
-      //   this.fileNameArr.push(fileName)
-      // },
-      // submitBefore(baseDialog, callBack) { // 多张图片拼路径
-      //   console.log('===FilePath=================', this.filePathArr)
-      //   let filePath = ''
-      //   if (this.filePathArr.length) {
-      //     this.filePathArr.forEach(e => {
-      //       filePath += e + ','
-      //     })
-      //     filePath = filePath.substr(0, filePath.length - 1)
-      //   }
-      //   let fileName = ''
-      //   if (this.fileNameArr.length) {
-      //     this.fileNameArr.forEach(e => {
-      //       fileName += e + ','
-      //     })
-      //     fileName = fileName.substr(0, fileName.length - 1)
-      //   }
-      //
-      //   baseDialog.formModel.photo = filePath
-      //   baseDialog.formModel.imgName = fileName
-      //   callBack()
-      // },
       setGroup(item) {
         this.formModel.downlosd = item.albumname
       },
-      getModelDia(val) { // 诊断三级联动
-        this.formModel.diagnosis = val.toString()
-        console.log('diagnosis=========', this.formModel.diagnosis)
-      },
-      getModelSur(val) { // 术式三级联动
-        this.formModel.surgical = val.toString()
-        console.log('surgical=========', this.formModel.surgical)
-      },
       submitForm() { // 保存按钮点击事件
+        console.log('this.formModel.diagnosis==================', this.formModel.diagnosis)
         if (this.submitCustom && typeof (this.submitCustom) === 'function') {
           this.submitCustom(this)
         } else if (this.submitBefore && typeof (this.submitBefore) === 'function') {
@@ -152,6 +96,26 @@
       },
       resetForm() { // 重置按钮
         this.$refs['formModel'].resetFields() // 重置信息
+      },
+      getDiaCascader() { // 获取诊断信息并以级联形式显示
+        console.log('getDiaCascader========================')
+        this.axios.request({
+          method: 'GET',
+          url: JdyydiaURL + '/getDiaCascader'
+        }).then(res => {
+          console.log('Request-Cascader-Success==============', res.data.data)
+          this.options = res.data.data
+        })
+      },
+      getSurCascader() { // 获取术式信息并以级联形式显示
+        console.log('getSurCascader========================')
+        this.axios.request({
+          method: 'GET',
+          url: JdyysurURL + '/getSurCascader'
+        }).then(res => {
+          console.log('Request-getSurCascader-Success==============', res.data.data)
+          this.items = res.data.data
+        })
       }
     }
   }
